465a4acfdfc27192691e9093c002cb2f8f0cf626
MattMakesNoise <72995030+MattMakesNoise@users.noreply.github.com>
2021-11-05 16:05:08 +0000

    makes burger and logo change colour once hero scrolled past

---
+++ dev-saved/app.es5.js
@@ -0,0 +1,87 @@
+"use strict";
+
+//=======================================================================//
+//=======================================================================//
+//===== TYPEWRITER EFFECT ON HERO BANNER ================================//
+//=======================================================================//
+//=======================================================================//
+//array to hold the text to be typed
+var text = ["human...", "legend...", "web developer!"];
+var a = 0;
+var b = 1;
+var reverse = false;
+var interval = setInterval(type, 150);
+
+function type() {
+  var nowText = text[a];
+  document.getElementById("typewriter").innerText = nowText.substring(0, b); //reverse direction (delete) if end of string reached
+
+  if (b == nowText.length + 5) {
+    //+5 makes it stall for a moment before typing
+    reverse = true;
+  }
+
+  b += reverse ? -2 : 1; //reset b and go forward again
+
+  if (b <= 0) {
+    b = 1;
+    reverse = false;
+    a++;
+  } //reached end of list then start over 
+
+
+  if (a == text.length) {
+    setTimeout(function () {
+      a = 0;
+    }, 5000);
+  }
+}
+
+; //=======================================================================//
+//=======================================================================//
+//===== CHANGE COLOUR OF LOGO AND BURGER DEPENDING ON PAGE BACKROUND ====//
+//=======================================================================//
+//=======================================================================//
+
+var logo = document.querySelector('.logo');
+var logoBorder = document.querySelector('.logo-wrapper');
+var burgerOne = document.querySelector('.burger-lineOne');
+var burgerTwo = document.querySelector('.burger-lineTwo');
+var burgerThree = document.querySelector('.burger-lineThree');
+var heroSection = document.querySelector('.hero-wrapper');
+var heroSectionOptions = {
+  root: null,
+  //it is the viewport
+  threshold: 0,
+  rootMargin: "0px"
+};
+var heroSectionObserver = new IntersectionObserver(function (entries, heroSectionObserver) {
+  entries.forEach(function (entry) {
+    if (!entry.isIntersecting) {
+      logoBorder.classList.add('logo-wrapper-dark');
+      logo.classList.add('logo-dark');
+      burgerOne.classList.add('burger-dark');
+      burgerTwo.classList.add('burger-dark');
+      burgerThree.classList.add('burger-dark');
+      console.log("Colurs should change!");
+    } else {
+      logoBorder.classList.remove('logo-wrapper-dark');
+      logo.classList.remove('logo-dark');
+      burgerOne.classList.remove('burger-dark');
+      burgerTwo.classList.remove('burger-dark');
+      burgerThree.classList.remove('burger-dark');
+    }
+
+    console.log(entry);
+  });
+}, heroSectionOptions);
+heroSectionObserver.observe(heroSection); //=======================================================================//
+//=======================================================================//
+//===== BRING CARDS IN FROM THE SIDE AS THE PAGE IS SCROLLED ============//
+//=======================================================================//
+//=======================================================================//
+//=======================================================================//
+//=======================================================================//
+//===== CONTACT FORM VALIDATION =========================================//
+//=======================================================================//
+//=======================================================================//
--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+//=======================================================================//
+//=======================================================================//
+//===== TYPEWRITER EFFECT ON HERO BANNER ================================//
+//=======================================================================//
+//=======================================================================//
+
 //array to hold the text to be typed
 let text = ["human..." , "legend..." , "web developer!"];
 
@@ -8,7 +14,7 @@ let interval = setInterval(type, 150);
 
 function type() {
     let nowText = text[a];
-    document.getElementById("typewriter").innerText = nowText.substr(0 , b);
+    document.getElementById("typewriter").innerText = nowText.substring(0 , b);
     //reverse direction (delete) if end of string reached
     if (b == nowText.length + 5) { //+5 makes it stall for a moment before typing
         reverse = true;
@@ -27,3 +33,65 @@ function type() {
         
     } 
 };
+
+//=======================================================================//
+//=======================================================================//
+//===== CHANGE COLOUR OF LOGO AND BURGER DEPENDING ON PAGE BACKROUND ====//
+//=======================================================================//
+//=======================================================================//
+
+const logo = document.querySelector('.logo');
+const logoBorder = document.querySelector('.logo-wrapper');
+const burgerOne = document.querySelector('.burger-lineOne');
+const burgerTwo = document.querySelector('.burger-lineTwo');
+const burgerThree = document.querySelector('.burger-lineThree');
+const heroSection = document.querySelector('.hero-wrapper');
+
+const heroSectionOptions = {
+    root: null, //it is the viewport
+    threshold: 0,
+    rootMargin: "0px"
+};
+
+const heroSectionObserver = new IntersectionObserver(function(entries, heroSectionObserver) {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            logoBorder.classList.add('logo-wrapper-dark');
+            logo.classList.add('logo-dark');
+            burgerOne.classList.add('burger-dark');
+            burgerTwo.classList.add('burger-dark');
+            burgerThree.classList.add('burger-dark');
+            console.log("Colurs should change!");
+        } else {
+            logoBorder.classList.remove('logo-wrapper-dark');
+            logo.classList.remove('logo-dark');
+            burgerOne.classList.remove('burger-dark');
+            burgerTwo.classList.remove('burger-dark');
+            burgerThree.classList.remove('burger-dark');
+        }
+        console.log(entry);
+    });
+}, heroSectionOptions);
+
+heroSectionObserver.observe(heroSection);
+
+
+
+
+
+
+
+//=======================================================================//
+//=======================================================================//
+//===== BRING CARDS IN FROM THE SIDE AS THE PAGE IS SCROLLED ============//
+//=======================================================================//
+//=======================================================================//
+
+
+
+
+//=======================================================================//
+//=======================================================================//
+//===== CONTACT FORM VALIDATION =========================================//
+//=======================================================================//
+//=======================================================================//
